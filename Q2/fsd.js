d166be7f28d8ef61f0da5b761554c2936e5dc319
vinayak7276 <vinayakpatil7350777485@gmail.com>
2025-10-17 11:11:53 +0530

    Add files via upload

---
+++ Q2/fsd.js
@@ -0,0 +1,11 @@
+let size="200";
+
+if(size==="xl"){
+    console.log("price is 250");
+}else if(size==="l"){
+    console.log("price is 200");
+}else if(size=="m"){
+    console.log("price is 100");
+}else{
+    console.log("price is 50");
+}
