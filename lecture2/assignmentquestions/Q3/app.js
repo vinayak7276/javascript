b61931de961ab4db035e4ae3517b1249b8c30d12
vinayak7276 <vinayakpatil7350777485@gmail.com>
2025-10-21 11:04:15 +0530

    Add files via upload

---
+++ lecture2/assignmentquestions/Q3/app.js
@@ -0,0 +1,17 @@
+let quarter=1;
+switch(quarter){
+    case 1:
+        console.log("January","February","March","April");
+        break;
+        case 2:
+            console.log("May","june");
+            break;
+            case 3:
+                console.log("July","August","September");
+                break;
+                case 4:
+                    console.log("October","November","December");
+                    break;
+                    default:
+                        console.log("Not a quarter");
+}
